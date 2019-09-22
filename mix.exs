8d6352c68da9db8d1ed97864ba02dceba4fd45d3
Luis F M Alves <luis.projensino@gmail.com>
2019-09-22 16:45:55 -0300

    fix: increment operator on dependency ecto_sql

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule AuctionUmbrella.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
-      {:ecto_sql, "3.2.0"}
+      {:ecto_sql, "~> 3.2.0"}
     ]
   end
 end
