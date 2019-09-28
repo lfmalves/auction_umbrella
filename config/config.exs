7b3d406507a1b7403cf101e85633c1b9e62d25bf
Luis F M Alves <luis.projensino@gmail.com>
2019-09-28 09:22:42 -0300

    feat: Auction.Repo into config.exs

--- config/config.exs
+++ config/config.exs
@@ -37,3 +37,5 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :auction, ecto_repos: [Auction.Repo]
